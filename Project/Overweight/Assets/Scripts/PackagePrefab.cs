5ca04c885b25a3367727790867cb3120edd1d327
alexander.correia <alexander.correia@creative-assembly.com>
2019-09-20 22:37:30 +0100

    Basic package prefabs setup

---
+++ Project/Overweight/Assets/Scripts/PackagePrefab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackagePrefab : MonoBehaviour
+{
+    GameObject package;
+    
+    void Start()
+    {
+        
+    }
+
+}
