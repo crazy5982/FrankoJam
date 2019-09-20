90c6925fffd516de10f2d7d466e50252670b7cf2
Faceinstien <35467169+Faceinstien@users.noreply.github.com>
2019-09-20 23:01:34 +0100

    Making art folder appear

--- Project/Overweight/Assets/Scripts/GameController.cs
+++ Project/Overweight/Assets/Scripts/GameController.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject player1;
-    public GameObject player2;
-    public GameObject player3;
-    public GameObject player4;
+    public GameObject player1Scale;
+    public GameObject player2Scale;
+    public GameObject player3Scale;
+    public GameObject player4Scale;
     private float timer = 3;
     public int timeLimit = 20;
     private int timerAsInt;
     public Text timerUI;
     private int objectiveWeight = 10;
-    private int player1Scale;
-    private int player2Scale;
-    private int player3Scale;
-    private int player4Scale;
+    private int player1Score;
+    private int player2Score;
+    private int player3Score;
+    private int player4Score;
 
 
 
@@ -28,6 +29,12 @@ public class GameController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        CalculateTimer();
+
+    }
+
+    void CalculateTimer()
     {
         if (timer >= 0)
         {
@@ -40,13 +47,10 @@ public class GameController : MonoBehaviour
         {
             timerUI.text = "Time is up mother lickers!";
         }
-
-
     }
 
-    void CalculateTimer()
+    void GetPlayerScales()
     {
 
-        
     }
 }
