5fa0a0cef897266604cbcd00ff08cc329a8882ba
MegadanXzero <megadanxzero@hotmail.com>
2019-09-21 17:25:31 +0100

    Added ScaleZones and various changes to allow using them

--- Project/Overweight/Assets/Scripts/parcel.cs
+++ Project/Overweight/Assets/Scripts/parcel.cs
@@ -6,4 +6,25 @@ public class parcel : MonoBehaviour
 {
     // Package weight variable setup
     [SerializeField] int parcelWeight;
+	public int ParcelWeight
+	{
+		get { return parcelWeight; }
+	}
+
+	private ScaleZone m_ScaleZone = null;
+
+	public void SetScaleZone(ScaleZone scaleZone)
+	{
+		m_ScaleZone = scaleZone;
+	}
+
+	public bool RemovePackageFromZone(int playerIndex)
+	{
+		if (m_ScaleZone != null)
+		{
+			return m_ScaleZone.RemovePackage(this, playerIndex);
+		}
+
+		return true;
+	}
 }
