df89e83e919b7af61211d0e84bf45ee43e293592
alexander.correia <alexander.correia@creative-assembly.com>
2019-09-21 16:34:18 +0100

    Adding initial force to parcels

--- Project/Overweight/Assets/Scripts/parcel.cs
+++ Project/Overweight/Assets/Scripts/parcel.cs
@@ -6,4 +6,18 @@ public class parcel : MonoBehaviour
 {
     // Package weight variable setup
     [SerializeField] int parcelWeight;
+
+    [SerializeField]
+    protected float m_ThrowSpeedH = 0.50f;
+    [SerializeField]
+    protected float m_ThrowSpeedV = 0.10f;
+
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Vector3 throwVector = new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2));
+        rb.velocity = throwVector;
+    }
 }
